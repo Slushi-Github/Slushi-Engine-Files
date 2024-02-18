e647556d1b9760295a983b5f5e6934e884f905bb
Andres_Github <81278332+Slushi-Github@users.noreply.github.com>
2024-02-17 20:50:36 -0400

    Update SLELuaWiki.lua

--- SLELuaWiki.lua
+++ SLELuaWiki.lua
@@ -1,16 +1,15 @@
 --[[
 Este es el documento de todas las funciones Lua existentes dentro de Slushi Engine, todo por mi, Slushi.
 
-VERSION DEL DOCUMENTO = v1.2.3 -- 23/10/2023
+VERSION DEL DOCUMENTO = v1.3.0 -- 17/2/2024
 
 [LOG:
-null
+null.
 ]
 
 [HECHO EN BASE A:
-VERSION DE PE: 0.7.1h -- VERSION DE SLE: 0.1.5 [Beta] -- VERSION DE SCE: 0.2.3 Beta]
+VERSION DE PE: 0.7.3h -- VERSION DE SLE: 0.1.9 -- VERSION DE SCE: 0.1.4]
 ]]--
-
 
 --[[
 Estos avisos estaran al final de cada funcion Lua o incluso variable, incidan la compatiblidad y estado de la funcion
@@ -25,14 +24,14 @@ Lua dependiendiendo de ciertos casos:
 ]]--
 
 --[[
-Todas, y digo TODAS, estas funciones que veran aca, se necesita que el modo developer este activado.
+Todas, y digo TODAS, estas funciones que veran aca, se necesita que el modo developer este activado. -- YA NO! :3
 ]]--
 
 -- FUNCIONES HARDCODEADAS DEL FNF: -----------------
-function winTrans(Trans, Black)
+function windowTrans(Trans, camToApply)
 -- hace la ventana del juego Transparente.
 -- trans = activa o desactiva la transparencia =  (true/false).
--- Black = coloca o no un fondo negro en CamGame = (true/false).
+-- camToApply = coloca o no un fondo negro en la camara que gustes.
 end -- [SCE_NULL -- <SLE_VER]
 
 function WindowAlpha(value)
@@ -49,6 +48,10 @@ end -- [>SLE_VER]
 
 function centerWindow()
 -- centra la ventana del juego en la pantalla.
+end -- [>SLE_VER]
+
+function resetAllCPPFunctions()
+-- Reinicia todas las funciones de Hxcpp de Windows.
 end -- [>SLE_VER]
 
 function getRAM()
@@ -70,9 +73,14 @@ function setWallpaper(image)
 -- image = el nombre de la imagen a poner, que debe estar en mods/images/wallpapers.
 end -- [>SLE_VER -- WARNING]
 
+function winScreenCapture(nameToSave)
+-- Toma una captura de toda la pantalla del monitor
+-- nameToSave = el nombre de la imagen a guardar en mods/images/winAssets.
+end -- [>SLE_VER]
+
 function getLanguage()
 -- obten el idioma en el que se configuro el juego, para traducir textos y ese tipo de cosas.
-end -- [>SLE_VER]
+end -- [<SLE_VER]
 
 function winAlert(text, title)
 -- crea una alerta en forma de ventana.
@@ -109,7 +117,8 @@ end -- [>SLE_VER]
 
 function winTitle(text)
 -- cambia el nombre de la ventana del juego.
--- text = el nuevo titulo de la ventana, el valor "text", si se le pone "default", (si estas en en una version de SLE inferior a la v0.1.0 es "titledef") se volvera al titulo original.
+-- text = el nuevo titulo de la ventana, el valor "text", si se le pone "default", 
+-- (si estas en en una version de SLE inferior a la v0.1.0 es "titledef") se volvera al titulo original.
 end -- [<>SLE_VER -- SCE_NULL -- NO_RECOM...?]
 
 function setWinPosX(X)
@@ -120,6 +129,50 @@ end -- [>SLE_VER]
 function setWinPosY(Y)
 -- mueve la ventana en la cordenada Y.
 -- X = la cordenada Y
+end -- [>SLE_VER]
+
+function moveDesktopWindowsInX(X)
+-- mueve los iconos del escritorio en la cordenada X.
+-- X = la cordenada X
+end -- [>SLE_VER]
+
+function moveDesktopWindowsInY(Y)
+-- mueve los iconos del escritorio en la cordenada Y.
+-- X = la cordenada Y
+end -- [>SLE_VER]
+
+function moveDesktopWindowsInXY(XY)
+-- mueve los iconos del escritorio en la cordenada X y Y.
+-- XY = la cordenadas
+end -- [>SLE_VER]
+
+function doTweenDesktopWindows(mode, toValue, duration, ease)
+-- mueve los iconos del escritorio con un tween.
+-- mode = que valor se modificara (X o Y).
+-- toValue = valor en la cordenada Y.
+-- duration = tiempo del tween.
+-- ease = tipo de ease. 
+end -- [>SLE_VER]
+
+function doTweenTaskBarAlpha(fromValue, toValue, duration, ease)
+-- cambia el alpha de la barra de tareas de Windows con un tween.
+-- fromValue = el valor inicial.
+-- toValue: = el valor al que ira el alpha.
+-- duration = tiempo del tween.
+-- ease = tipo de ease. 
+end -- [>SLE_VER]
+
+function doTweenDesktopWindowsAlpha(fromValue, toValue, duration, ease)
+-- cambia el alpha de los iconos del escritorio pero con un tween.
+-- fromValue = el valor inicial.
+-- toValue: = el valor al que ira el alpha.
+-- duration = tiempo del tween.
+-- ease = tipo de ease. 
+end -- [>SLE_VER]
+
+function getDesktopWindowsPos(mode)
+-- obten el valor de la posiciones de los iconos del escritorio.
+-- mode: X o Y
 end -- [>SLE_VER]
 
 function gameCrash(error, realCrash)
@@ -156,4 +209,3 @@ end -- [>SLE_VER]
 -- esto, quiere decir que es una funcion de Lua ubicada en algun script Lua existente en mods/scripts.
 -- nada, por los momeentos.
 ------------------------------------------------------------------------------------------
-
